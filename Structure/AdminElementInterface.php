cdd1c77f2118417114c2f33bae5033134ec91a1a
Norbert Orzechowicz <norbert.orzechowicz@fsi.pl>
2013-07-12 13:00:41 +0200

    Removed useless use statements

--- Structure/AdminElementInterface.php
+++ Structure/AdminElementInterface.php
@@ -9,9 +9,7 @@
 
 namespace FSi\Bundle\AdminBundle\Structure;
 
-use FSi\Bundle\AdminBundle\Exception\RuntimeException;
 use FSi\Component\DataGrid\DataGridFactoryInterface;
-use FSi\Component\DataIndexer\DoctrineDataIndexer;
 use FSi\Component\DataSource\DataSourceFactoryInterface;
 use Symfony\Component\Form\FormFactoryInterface;
 
