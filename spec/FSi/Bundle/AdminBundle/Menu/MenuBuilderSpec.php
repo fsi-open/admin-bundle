0ae499003b75b25ce2d78b2e3ced431838c05571
Norbert Orzechowicz <norbert@orzechowicz.pl>
2014-02-11 19:13:29 +0100

    Fixed menu specs

--- spec/FSi/Bundle/AdminBundle/Menu/MenuBuilderSpec.php
+++ spec/FSi/Bundle/AdminBundle/Menu/MenuBuilderSpec.php
@@ -30,12 +30,13 @@ class MenuBuilderSpec extends ObjectBehavior
     function it_create_menu_with_elements_without_group(
         MenuFactory $menuFactory,
         Manager $manager,
-        MenuItem $menu,
+        MenuItem $root,
+        MenuItem $menuItem,
         ElementInterface $element
     ) {
-        $menuFactory->createItem('root')->willReturn($menu);
-        $menu->setChildrenAttribute('class', 'nav navbar-nav')->shouldBeCalled();
-        $menu->setChildrenAttribute('id', 'top-menu')->shouldBeCalled();
+        $menuFactory->createItem('root')->willReturn($root);
+        $root->setChildrenAttribute('class', 'nav navbar-nav')->shouldBeCalled();
+        $root->setChildrenAttribute('id', 'top-menu')->shouldBeCalled();
 
         $manager->getElementsWithoutGroup()->willReturn(array($element));
         $manager->getGroups()->willReturn(array());
@@ -46,31 +47,34 @@ class MenuBuilderSpec extends ObjectBehavior
         $element->hasOption('menu')->willReturn(true);
         $element->getOption('menu')->willReturn(true);
 
-        $menu->addChild('element_name', array(
+        $root->addChild('element_name', array(
             'route' => 'fsi_admin_route',
             'routeParameters' => array(),
         ))->shouldBeCalled();
+        $root->offsetGet('element_name')->willReturn($menuItem);
+        $menuItem->setAttribute('class', 'admin-element')->shouldBeCalled();
 
-        $this->createMenu()->shouldReturn($menu);
+        $this->createMenu()->shouldReturn($root);
     }
 
     function it_create_menu_with_elements_in_groups(
         MenuFactory $menuFactory,
         Manager $manager,
-        MenuItem $menu,
-        MenuItem $subMenu,
+        MenuItem $root,
+        MenuItem $groupElement,
+        MenuItem $menuItem,
         ElementInterface $element
     ) {
-        $menuFactory->createItem('root')->willReturn($menu);
-        $menu->setChildrenAttribute('class', 'nav navbar-nav')->shouldBeCalled();
-        $menu->setChildrenAttribute('id', 'top-menu')->shouldBeCalled();
+        $menuFactory->createItem('root')->willReturn($root);
+        $root->setChildrenAttribute('class', 'nav navbar-nav')->shouldBeCalled();
+        $root->setChildrenAttribute('id', 'top-menu')->shouldBeCalled();
 
         $manager->getElementsWithoutGroup()->willReturn(array());
         $manager->getGroups()->willReturn(array('group'));
 
         $manager->getElementsByGroup('group')->willReturn(array($element));
-        $menu->addChild('group', array('uri' => '#'))->willReturn($menu);
-        $menu->setAttribute('dropdown', true)->shouldBeCalled();
+        $root->addChild('group', array('uri' => '#'))->willReturn($root);
+        $root->setAttribute('dropdown', true)->shouldBeCalled();
         $element->hasOption('menu')->willReturn(true);
         $element->getOption('menu')->willReturn(true);
 
@@ -78,24 +82,26 @@ class MenuBuilderSpec extends ObjectBehavior
         $element->getRoute()->willReturn('fsi_admin_route');
         $element->getRouteParameters()->willReturn(array());
 
-        $menu->offsetGet('group')->willReturn($subMenu);
-        $subMenu->addChild('element_name', array(
+        $root->offsetGet('group')->willReturn($groupElement);
+        $groupElement->addChild('element_name', array(
             'route' => 'fsi_admin_route',
             'routeParameters' => array(),
         ))->shouldBeCalled();
+        $groupElement->offsetGet('element_name')->willReturn($menuItem);
+        $menuItem->setAttribute('class', 'admin-element')->shouldBeCalled();
 
-        $this->createMenu()->shouldReturn($menu);
+        $this->createMenu()->shouldReturn($root);
     }
 
     function it_ignore_elements_without_menu_option(
         MenuFactory $menuFactory,
         Manager $manager,
-        MenuItem $menu,
+        MenuItem $root,
         ElementInterface $element
     ) {
-        $menuFactory->createItem('root')->willReturn($menu);
-        $menu->setChildrenAttribute('class', 'nav navbar-nav')->shouldBeCalled();
-        $menu->setChildrenAttribute('id', 'top-menu')->shouldBeCalled();
+        $menuFactory->createItem('root')->willReturn($root);
+        $root->setChildrenAttribute('class', 'nav navbar-nav')->shouldBeCalled();
+        $root->setChildrenAttribute('id', 'top-menu')->shouldBeCalled();
 
         $manager->getElementsWithoutGroup()->willReturn(array($element));
         $manager->getGroups()->willReturn(array());
@@ -106,23 +112,23 @@ class MenuBuilderSpec extends ObjectBehavior
         $element->hasOption('menu')->willReturn(false);
         $element->getOption('menu')->shouldNotBeCalled();
 
-        $menu->addChild('element_name', array(
+        $root->addChild('element_name', array(
             'route' => 'fsi_admin_route',
             'routeParameters' => array(),
         ))->shouldNotBeCalled();
 
-        $this->createMenu()->shouldReturn($menu);
+        $this->createMenu()->shouldReturn($root);
     }
 
     function it_ignore_elements_with_menu_option_value_false(
         MenuFactory $menuFactory,
         Manager $manager,
-        MenuItem $menu,
+        MenuItem $root,
         ElementInterface $element
     ) {
-        $menuFactory->createItem('root')->willReturn($menu);
-        $menu->setChildrenAttribute('class', 'nav navbar-nav')->shouldBeCalled();
-        $menu->setChildrenAttribute('id', 'top-menu')->shouldBeCalled();
+        $menuFactory->createItem('root')->willReturn($root);
+        $root->setChildrenAttribute('class', 'nav navbar-nav')->shouldBeCalled();
+        $root->setChildrenAttribute('id', 'top-menu')->shouldBeCalled();
 
         $manager->getElementsWithoutGroup()->willReturn(array($element));
         $manager->getGroups()->willReturn(array());
@@ -133,31 +139,31 @@ class MenuBuilderSpec extends ObjectBehavior
         $element->hasOption('menu')->willReturn(false);
         $element->getOption('menu')->willReturn(false);
 
-        $menu->addChild('element_name', array(
+        $root->addChild('element_name', array(
             'route' => 'fsi_admin_route',
             'routeParameters' => array(),
         ))->shouldNotBeCalled();
 
-        $this->createMenu()->shouldReturn($menu);
+        $this->createMenu()->shouldReturn($root);
     }
 
     function it_ignore_menu_elements_in_groups_without_menu_option(
         MenuFactory $menuFactory,
         Manager $manager,
-        MenuItem $menu,
-        MenuItem $subMenu,
+        MenuItem $root,
+        MenuItem $groupElement,
         ElementInterface $element
     ) {
-        $menuFactory->createItem('root')->willReturn($menu);
-        $menu->setChildrenAttribute('class', 'nav navbar-nav')->shouldBeCalled();
-        $menu->setChildrenAttribute('id', 'top-menu')->shouldBeCalled();
+        $menuFactory->createItem('root')->willReturn($root);
+        $root->setChildrenAttribute('class', 'nav navbar-nav')->shouldBeCalled();
+        $root->setChildrenAttribute('id', 'top-menu')->shouldBeCalled();
 
         $manager->getElementsWithoutGroup()->willReturn(array());
         $manager->getGroups()->willReturn(array('group'));
 
         $manager->getElementsByGroup('group')->willReturn(array($element));
-        $menu->addChild('group', array('uri' => '#'))->willReturn($menu);
-        $menu->setAttribute('dropdown', true)->shouldBeCalled();
+        $root->addChild('group', array('uri' => '#'))->willReturn($root);
+        $root->setAttribute('dropdown', true)->shouldBeCalled();
         $element->hasOption('menu')->willReturn(false);
         $element->getOption('menu')->shouldNotBeCalled();
 
@@ -165,28 +171,28 @@ class MenuBuilderSpec extends ObjectBehavior
         $element->getRoute()->willReturn('fsi_admin_route');
         $element->getRouteParameters()->willReturn(array());
 
-        $menu->offsetGet('group')->willReturn($subMenu);
+        $root->offsetGet('group')->willReturn($groupElement);
 
-        $this->createMenu()->shouldReturn($menu);
+        $this->createMenu()->shouldReturn($root);
     }
 
     function it_ignore_menu_elements_in_groups_with_menu_option_value_false(
         MenuFactory $menuFactory,
         Manager $manager,
-        MenuItem $menu,
-        MenuItem $subMenu,
+        MenuItem $root,
+        MenuItem $groupElement,
         ElementInterface $element
     ) {
-        $menuFactory->createItem('root')->willReturn($menu);
-        $menu->setChildrenAttribute('class', 'nav navbar-nav')->shouldBeCalled();
-        $menu->setChildrenAttribute('id', 'top-menu')->shouldBeCalled();
+        $menuFactory->createItem('root')->willReturn($root);
+        $root->setChildrenAttribute('class', 'nav navbar-nav')->shouldBeCalled();
+        $root->setChildrenAttribute('id', 'top-menu')->shouldBeCalled();
 
         $manager->getElementsWithoutGroup()->willReturn(array());
         $manager->getGroups()->willReturn(array('group'));
 
         $manager->getElementsByGroup('group')->willReturn(array($element));
-        $menu->addChild('group', array('uri' => '#'))->willReturn($menu);
-        $menu->setAttribute('dropdown', true)->shouldBeCalled();
+        $root->addChild('group', array('uri' => '#'))->willReturn($root);
+        $root->setAttribute('dropdown', true)->shouldBeCalled();
         $element->hasOption('menu')->willReturn(false);
         $element->getOption('menu')->willReturn(false);
 
@@ -194,8 +200,8 @@ class MenuBuilderSpec extends ObjectBehavior
         $element->getRoute()->willReturn('fsi_admin_route');
         $element->getRouteParameters()->willReturn(array());
 
-        $menu->offsetGet('group')->willReturn($subMenu);
+        $root->offsetGet('group')->willReturn($groupElement);
 
-        $this->createMenu()->shouldReturn($menu);
+        $this->createMenu()->shouldReturn($root);
     }
 }
