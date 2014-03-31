d9c41f8f48915570cb784268c08a7550e3b2128c
Norbert Orzechowicz <norbert@orzechowicz.pl>
2014-03-31 19:05:00 +0200

    Created ElementCollectionVisitor that allows to remove element creation logic from compiler pass

---
+++ Admin/Manager/ElementCollectionVisitor.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * (c) FSi sp. z o.o. <info@fsi.pl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace FSi\Bundle\AdminBundle\Admin\Manager;
+
+use Doctrine\Common\Annotations\AnnotationReader;
+use FSi\Bundle\AdminBundle\Admin\Manager;
+use FSi\Bundle\AdminBundle\Admin\ManagerInterface;
+use FSi\Bundle\AdminBundle\Annotation\Element;
+use FSi\Bundle\AdminBundle\Factory\ElementFactory;
+use FSi\Bundle\AdminBundle\Factory\ProductionLine;
+use FSi\Bundle\AdminBundle\Finder\AdminClassFinder;
+
+class ElementCollectionVisitor implements Visitor
+{
+    /**
+     * @var array
+     */
+    private $elements;
+
+    /**
+     * @var \FSi\Bundle\AdminBundle\Factory\ProductionLine
+     */
+    private $factoryProductionLine;
+
+    public function __construct($elements = array(), ProductionLine $factoryProductionLine)
+    {
+        $this->elements = $elements;
+        $this->factoryProductionLine = $factoryProductionLine;
+    }
+
+    /**
+     * @param ManagerInterface $manager
+     */
+    public function visitManager(ManagerInterface $manager)
+    {
+        foreach ($this->elements as $element) {
+            $this->factoryProductionLine->workOn($element);
+            $manager->addElement($element);
+        }
+    }
+}
