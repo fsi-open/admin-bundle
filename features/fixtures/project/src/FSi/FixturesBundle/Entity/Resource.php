32ff439cdab6f7972a192bc6aecda16ddb258b1a
Lukasz Cybula <lukasz@fsi.pl>
2017-10-27 10:53:43 +0200

    Do not use deprecated (and later removed) ResourceRepository class

--- features/fixtures/project/src/FSi/FixturesBundle/Entity/Resource.php
+++ features/fixtures/project/src/FSi/FixturesBundle/Entity/Resource.php
@@ -8,7 +8,7 @@ use Doctrine\ORM\Mapping as ORM;
 use FSi\Bundle\ResourceRepositoryBundle\Model\Resource as BaseResource;
 
 /**
- * @ORM\Entity(repositoryClass="FSi\Bundle\ResourceRepositoryBundle\Entity\ResourceRepository")
+ * @ORM\Entity(repositoryClass="FSi\Bundle\ResourceRepositoryBundle\Doctrine\ResourceRepository")
  * @ORM\Table(name="fsi_resource")
  */
 class Resource extends BaseResource
