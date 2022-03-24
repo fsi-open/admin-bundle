1dc22bd4a8ee7e492c66ba28d2168b8834420da2
Lukasz Cybula <lukasz@fsi.pl>
2022-03-24 17:18:54 +0100

    Added missing licenses

--- spec/FSi/Bundle/AdminBundle/Controller/AdminControllerSpec.php
+++ spec/FSi/Bundle/AdminBundle/Controller/AdminControllerSpec.php
@@ -1,5 +1,12 @@
 <?php
 
+/**
+ * (c) FSi sp. z o.o. <info@fsi.pl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 declare(strict_types=1);
 
 namespace spec\FSi\Bundle\AdminBundle\Controller;
